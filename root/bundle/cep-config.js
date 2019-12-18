6e37cc85c31d5eceb3999c470de3054b6afe5d67
Francesco Camarlinghi <me@minifloppy.it>
2019-12-18 16:28:11 +0100

    Support CC2020

--- root/bundle/cep-config.js
+++ root/bundle/cep-config.js
@@ -36,7 +36,7 @@
             bundle: { manifest: 'bundle/manifest.bundle.cc2017.xml' },
             products: {%= products %},
             source: 'src',
-            families: ['CC2019', 'CC2018', 'CC2017'],
+            families: ['CC2020', 'CC2019', 'CC2018', 'CC2017'],
         },
         // Legacy versions
         {
